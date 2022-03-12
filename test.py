0c6eb3743d4eb6cf4323477412f35293685f1724
Krishna Dhingra <dkrishna1608@gmail.com>
2022-03-13 03:08:03 +0530

    Practiced some basic stuff

---
+++ test.py
@@ -0,0 +1,22 @@
+def checkOddEven(num):
+    if num % 2 == 0:
+        return "The number is even"
+    else: 
+        return "The number is odd"
+# print(checkOddEven(2))
+
+# names = ['Krishna', 'Khushi', 'Sachin']
+# for name in names:
+#     if(name == 'Khushi'):
+#         break
+#     print(name)
+
+# def printTable(num):
+#     i = 1
+#     for i in range(1, 11):
+#         print(f'{num} X {i} = {num * i}')
+#         i+=1
+
+# printTable(3)
+
+
